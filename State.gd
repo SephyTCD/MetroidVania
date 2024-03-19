0cdb4a0c8026b71902063ce4ad6d0bf38a7f041b
LoganWu101 <158172600+LoganWu101@users.noreply.github.com>
2024-03-19 09:20:53 -0400

    Update State.gd

--- State.gd
+++ State.gd
@@ -1,11 +1,14 @@
 extends Node
+class_name StateM
 
+signal state_transition
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func Enter():
+	pass
+	
+	
+func Exit():
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func Update(_delta:float):
 	pass
